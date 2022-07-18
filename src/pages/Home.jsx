9a31f4d476107b49364cd9d12f14472796ddb11e
asimboga <asimboga@gmail.com>
2022-07-18 22:59:05 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Navbar from '../components/AppBarComp';
+import CardsGrid from '../components/CardsGrid';
+import TextFieldComp from '../components/TextFieldComp';
+import TypoButtons from '../components/TypoButtons';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      {/* <TypoButtons /> */}
+      {/* <TextFieldComp /> */}
+      <CardsGrid />
+    </>
+  );
+};
+
+export default Home;
